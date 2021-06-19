fb1ec5e18455e5fd3f5551b71892f14c4922f74a
ray_arwindra <wan.muhammad81@ui.ac.id>
2021-06-19 21:31:09 +0700

    Added landing page content

--- src/Landing.js
+++ src/Landing.js
@@ -1,9 +1,18 @@
+import { Typography } from "@material-ui/core";
+import useStyles from "./styles/landing.style";
+
 function Landing() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const classes = useStyles();
+  return (
+    <div className={classes.text}>
+      <Typography className={classes.heading} variant="h2" paragraph>
+        Hi! I'm Rayhan Arwindra 👋
+      </Typography>
+      <Typography  variant="h3" className={classes.subtitle}>
+          Student, and Aspiring Software Engineer
+      </Typography>
+    </div>
+  );
 }
 
 export default Landing;
---
+++ src/styles/landing.style.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    text: {
+        color: "white",
+        marginTop: "10%",
+        width: "80%",
+        padding: "0 100px"
+    },
+    heading: {
+        fontWeight: "bold",
+        marginBottom: "30px",
+    }
+});
+
+export default useStyles;
